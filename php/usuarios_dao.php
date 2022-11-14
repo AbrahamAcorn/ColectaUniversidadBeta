6f11ee64ffd97f7ad45ed8e69802751ea40f7830
AbrahamRiRa <abrahmr0@gmail.com>
2022-11-14 00:11:17 -0600

    login y crud para usuarios

---
+++ php/usuarios_dao.php
@@ -0,0 +1,60 @@
+<?php
+include('conexion_bd.php');
+
+class DonadorDAO
+{
+    private $conexion;
+
+    public function __construct()
+    {
+        $this->conexion = new ConexionBD();
+    }
+
+    //=====================METODOS PARA ABCC====================
+
+    //----------------------------ALTAS-------------------------
+    public function crearUsuario($id, $user, $email, $password)
+    {
+        $sql = "INSERT INTO users VALUES('', $user, $email, $password)";
+
+        if (mysqli_query($this->conexion->getConexion(), $sql)) {
+            //echo "<script> alert('Agregado con EXITO'); </script>"
+        } else {
+            //echo "¿SERA MUY TARDE PARA CAMBIAR DE CARRERA?   :(";
+        }
+    }
+
+    //----------------------------BAJAS-------------------------
+    public function eliminarUsuario($id)
+    {
+        $sql = "DELETE FROM users WHERE id ='$id'";
+
+        if (mysqli_query($this->conexion->getConexion(), $sql)) {
+            echo "PERFECTO, CASI SOY ISC :)";
+            //echo "<script> alert('Agregado con EXITO'); </script>"
+        } else {
+            //echo "¿SERA MUY TARDE PARA CAMBIAR DE CARRERA?   :(";
+        }
+    }
+
+    //----------------------------CAMBIOS-------------------------
+    public function cambioUsuario($id, $user, $email, $password)
+    {
+        $sql = "UPDATE users SET user='$user', email='$email', password=$password WHERE id='$id'";
+
+        if (mysqli_query($this->conexion->getConexion(), $sql)) {
+            header("location:../vista/formulario_consultas.php");
+        } else {;
+            echo mysqli_error($this->conexion->getConexion());
+        }
+    }
+
+    //----------------------------CONSULTAS-------------------------
+    public function cargarUsuarios()
+    {
+        $sql = "SELECT * FROM users;";
+
+        return mysqli_query($this->conexion->getConexion(), $sql);
+    }
+
+}
